4d9b5756540d14df4186f0432e1f463c177d208e
Zayan Sheikh <dbzayan@gmail.com>
2024-05-17 22:48:08 -0700

    Designated routes for API

---
+++ server/routes/exercises.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const Exercise = require('../models/exerciseModel')
+
+const {
+    getExercises,
+    getExercise,
+    createExercise,
+    updateExercise,
+    deleteExercise
+} = require('../controllers/exerciseController')
+
+const router = express.Router()
+
+// get all exercises
+router.get('/', getExercises)
+
+// get one exercise
+router.get('/":id', getExercise)
+
+// update an exercise
+router.patch('/:id', updateExercise)
+
+// delete an exercise
+router.delete('/:id', deleteExercise)
+
+// create an exercise
+router.post('/', createExercise)
