a0832d883af635d180c384b8a972edd238334769
Kavya Dalmia <dalmia.kavya@gmail.com>
2024-05-24 13:05:25 -0700

    upload form

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,35 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // pages & components
-import Home from './pages/Home'
-import Navbar from './components/Navbar'
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import FormChecking from './pages/FormChecking';
+import Success from './pages/Success';
+
+import cors from 'cors';
+
+const corsOptions = {
+   origin: 'http://localhost:3000',
+   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+   credentials: true,
+   optionsSuccessStatus: 204,
+};
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar/>
+        <Navbar />
         <div className='pages'>
           <Routes>
-            <Route
-              path='/'
-              element={<Home/>}
-            />
+            <Route path='/' element={<Home />} />
+            <Route path='/formChecking' element={<FormChecking />} />
+            <Route path='/success' element={<Success />} />
           </Routes>
         </div>
       </BrowserRouter>
     </div>
   );
 }
-
+// App.use(cors(corsOptions)); 
 export default App;
